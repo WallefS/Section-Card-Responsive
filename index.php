332b0bab245909c2a36c7845385b3fe0a3a2f8f6
wallef <wallefsousa1@gmail.com>
2019-03-13 12:32:05 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Card Responsive</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<br><br><br>
+	<div class="pricing-card">
+
+		<div class="col">
+			<div class="table">
+				<h2>Básico</h2>
+
+				<div class="gratis">Grátis</div>
+
+
+				<div class="price">$0
+					<span>Por Mês</span>
+				</div>
+
+				<ul>
+					<li><strong>1</strong> Websites</li>
+					<li><strong>1</strong> Emails</li>
+					<li><strong>2GB</strong> Storage</li>
+					<li><strong>1</strong> Account</li>
+				</ul>
+				<a href="#">Compre-já</a>
+			</div>
+		</div>
+
+		<div class="col">
+			<div class="table">
+				<h2>Regular</h2>
+				<div class="price">$10
+					<span>Por Mês</span>
+				</div>
+
+				<ul>
+					<li><strong>2</strong> Websites</li>
+					<li><strong>10</strong> Emails</li>
+					<li><strong>10GB</strong> Storage</li>
+					<li><strong>1</strong> Account</li>
+				</ul>
+				<a href="#">Compre-já</a>
+			</div>
+		</div>
+
+		<div class="col">
+			<div class="table">
+				<h2>Médio</h2>
+
+				<div class="popular">Popular</div>
+
+				<div class="price">
+
+					$20
+					<span>Compre-já</span>
+				</div>
+
+
+				<ul>
+					<li><strong>2</strong> Websites</li>
+					<li><strong>15</strong> Emails</li>
+					<li><strong>30GB</strong> Storage</li>
+					<li><strong>10</strong> Account</li>
+				</ul>
+
+				<a href="#">Compre-já</a>
+			</div>
+		</div>
+		<div class="col">
+
+			<div class="table">
+				<h2>Profissional</h2>
+
+				<div class="Pro">Profissional</div>
+
+				<div class="price">
+
+					$40
+					<span>Por Mês</span>
+				</div>
+				<ul>
+					<li><strong>10</strong> Websites</li>
+					<li><strong>15</strong> Emails</li>
+					<li><strong>1000GB</strong> Storage</li>
+					<li><strong>20</strong> Account</li>
+				</ul>
+
+				<a href="#">Compre-já</a>
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
